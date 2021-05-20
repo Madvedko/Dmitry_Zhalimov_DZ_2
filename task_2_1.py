3e50f4071e0c1ff45bd5f053dfaa39d65eb887f0
Madvedko <madweretbsp7@gmail.com>
2021-05-20 17:18:11 +0300

    DZ_2

---
+++ task_2_1.py
@@ -0,0 +1,12 @@
+expression_one = 15 * 3
+print(type(expression_one))
+
+expression_two = 15 / 3
+print(type(expression_two))
+
+expression_three = 15 // 2
+print(type(expression_three))
+
+expression_four = 15 ** 2
+print(type(expression_four))
+
